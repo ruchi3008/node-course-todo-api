543634f562efe99c871285234dc85053f148ec5b
Ruchi <ruchi@localhost.localdomain>
2017-09-12 14:27:58 -0700

    Add GET URL param

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+
 
 var app = express();
 var port = process.env.PORT||3000;
@@ -27,6 +28,18 @@ app.get('/todos',(req,res) =>{
     res.status(400).send(e);
   });
 });
+
+app.get('/todos/:id',(req,res) => {
+  if(!ObjectID.isValid(req.params.id)){
+    return res.status(404).send();
+  }
+  Todo.findById(req.params.id).then((todo) => {
+    if(!todo){
+      res.send(404).send();
+    }
+    res.send({todo});
+  }).catch((e) => res.status(400).send());
+})
 app.listen(port,() => {
   console.log(`Server started on ${port}`);
 });
