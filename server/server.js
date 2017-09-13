89703fca7b2503b19684bdc78eea64ecc7d3ce7e
Ruchi <ruchi@localhost.localdomain>
2017-09-12 23:56:22 -0700

    Setup separate test and dev env

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('./config/config.js');
+
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -8,7 +10,7 @@ var {User} = require('./models/user');
 
 
 var app = express();
-var port = process.env.PORT||3000;
+var port = process.env.PORT;
 
 app.use(bodyParser.json());
 app.post('/todos',(req,res) => {
