07172eb6b4e2c3b56aca4f6c927de092991b79a1
Ruchi <ruchi@localhost.localdomain>
2017-09-13 16:05:20 -0700

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,29 @@
 const jwt = require('jsonwebtoken');
 const {SHA256} = require('crypto-js');
-var msg = 'I am stupid';
-var hash = SHA256(msg);
-console.log(`${msg}`);
-console.log(`${hash}`);
+const bcrypt = require('bcryptjs');
 
-var data ={
-  id:4
-};
+var password = '123abc';
 
-var token = jwt.sign(data,'123abc');
-console.log(`${token}`);
+// bcrypt.genSalt(10,(err,salt) => {
+//   bcrypt.hash(password,salt,(err,hash) =>{
+//     console.log(hash);
+//   });
+// });
 
-var data = jwt.verify(token,'123abc');
-console.log(data);
+bcrypt.compare('123abc','$2a$10$1498P8WIZT2wn6S7DHeoA.Of2hvNQcs7Di8.sKWYcI5AmX4eo/u1y',(err,res)=>{
+  console.log(res);
+});
+// var msg = 'I am stupid';
+// var hash = SHA256(msg);
+// console.log(`${msg}`);
+// console.log(`${hash}`);
+//
+// var data ={
+//   id:4
+// };
+//
+// var token = jwt.sign(data,'123abc');
+// console.log(`${token}`);
+//
+// var data = jwt.verify(token,'123abc');
+// console.log(data);
