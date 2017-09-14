7f6efdf0008c80408f79cf0065a538e3eb4e0f01
Ruchi <ruchi@localhost.localdomain>
2017-09-14 13:06:20 -0700

    DELETE token logout

--- server/models/user.js
+++ server/models/user.js
@@ -49,6 +49,16 @@ UserSchema.methods.generateAuthToken = function(){
   });
 };
 
+UserSchema.methods.removeToken = function (token) {
+var user = this;
+
+return user.update({
+  $pull:{
+    tokens: {token}
+  }
+});
+};
+
 UserSchema.statics.findByToken = function (token) {
   var user = this;
   var decodedToken;
@@ -84,6 +94,7 @@ UserSchema.statics.findByCredentials = function(email,password){
     });
   });
 };
+
 
 UserSchema.pre('save',function(next){
 var user = this;
